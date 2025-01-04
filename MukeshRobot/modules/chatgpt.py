91d1a4ef8942b527f6b403ab6ab3b9e0d04cb787
SHASHANK SHUKLA <shashankshukla69204@gmail.com>
2025-01-04 23:22:09 +0530

    Update chatgpt.py

--- MukeshRobot/modules/chatgpt.py
+++ MukeshRobot/modules/chatgpt.py
@@ -10,7 +10,7 @@ BASE_URL = "https://api.together.xyz/v1/chat/completions"
 
 @Mukesh.on_message(
     filters.command(
-        ["mchatgpt", "mai", "mask", "mgpt", "msolve"],
+        ["mchammtgpt", "mmmmai", "mammsk", "mgmmpt", "msommmlve"],
         prefixes=["+", ".", "/", "-", "", "$", "#", "&"],
     )
 )
